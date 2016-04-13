873d0a857c9d2bc05a95734874e4126495e6ffd7
grrc2016_1 <rod8902@gmail.com>
2016-04-13 19:17:23 +0900

    hpf_cuda_jack

--- cuda-jack/firhpf.cu
+++ cuda-jack/firhpf.cu
@@ -1,270 +1,63 @@
-/* dsp.cu */
-
-
 #include <stdio.h>
 #include <math.h>
-
 #include <jack/jack.h>
-#define BLOCK_SIZE 512
-#define KERNELTAPS	256	//must be odd value. 
-//Freq/Taps = Filter Frequency accuracy. 
-//5.4Hz for 44100.
 
-#define THREAD_NUM 512	// executed thread count per block, do not change. 
-// shared memory is common in the block.
+#define KERNELTAPS	256	//must be odd value. 
+						//Freq/Taps = Filter Frequency accuracy. 
+						//5.4Hz for 44100.
+
+#define BLOCKS	64	//4
+#define THREAD_NUM	32	// executed thread count per block, do not change. 
+						// shared memory is common in the block.
 
 #define DATAPERCYCLE 64  // data count per loop. do not change
 
-#define BLOCKS 4
-#define THREADS 512
-
-#define N  255
-/**********************************************************************/
-#define NUM_TAPS	51
-#define SAMPLERATE	44100
-#define FX	2.0
-
-__device__  float coeff_Kernel[KERNELTAPS];
-
-__global__ void calcFIR(const float * g_indata, float * g_outdata, const int nframes)
+__global__ void do_sample(const float *g_indata, float *g_outdata, float *g_sr, float *g_taps, int g_num_taps)
 {
-		/************************************************************************************/
-		// access Block Width
-		//const unsigned int bw = gridDim.x;
-		// access Block ID
-		//const unsigned int bix = blockIdx.x;
-
-		// access thread id
-		//	const unsigned int tid = threadIdx.x;
-
-		//int bx = blockIdx.x;
-		//int tx = threadIdx.x;
-		//int x = blockIdx.x * blockDim.x + threadIdx.x;
-		int x = blockIdx.x*blockDim.x + threadIdx.x;
-		/* 
-		   for (int i=0; i<nframes; i++) {
-		   g_outdata[i]=0.0f;
-		   for (int j=0; j<KERNELTAPS; j++) {
-		   g_outdata[i]+=h[j]*g_indata[i+j];
-		   }
-		   }
-		 */		//      loop over i and j:
-		//	out[i]+=h[j]*in[i+j];
-		//__shared__ float buf[N];
-		
-		float twopioversamplerate = (2*M_PI)/ 44100;	//rod
-		float comp;	//rod
-		float amountoflast, amountofcurrent;
-		int cutoff = 500;
-
-		comp = 2 - cos(twopioversamplerate * cutoff);
-		amountoflast = comp - (float)sqrt( comp * comp -1);
-		amountofcurrent = 1 - amountoflast;
-
-		g_outdata[x]=0.0f;
-		
-		if(x+N>nframes) {
-			return;
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	int i;
+	float result;
+	
+	float sr[32];
+	int idx = x+1;
+	
+	// For threads of block 0
+	if( x>=0 && x<g_num_taps){
+		for(i=0; i< g_num_taps; i++){
+			sr[i] = g_sr[i];
 		}
-#pragma unroll
-		for (int j=0; j<N-1; j++) {
-				//buf[j]=g_indata[x];
-				if(x+j>nframes) {
-						__syncthreads();
-						return;
-				} else {
-						//g_outdata[x] = g_outdata[x] + g_indata[(x+j)]*h[j]/(M_PI);
-						//g_outdata[x] = g_outdata[x]*amountoflast + (g_indata[(x+j)]*amountofcurrent)/(M_PI);
-						g_outdata[x] = g_outdata[x]*amountoflast + g_indata[(x+j)]*(1-amountofcurrent);
-				}
+	
+		for(i = g_num_taps - 1; i >= idx; i--){
+			sr[i] = sr[i-idx];
 		}
-		__syncthreads();
-}
 
-/*
-
-//do FIR
-//each threads has offseted address to global memory. loop jumps threads*blocks.
-for (int index = 0; index < nframes; index = index + THREAD_NUM*bw)
-{
-float dOut = 0.0;
-//x[oldest]=g_indata[index];
-
-//read g_indata to Shared Memory
-//cycle is, ex, 8=8192/1024.
-
-for (int j = 0; j < KERNELTAPS/DATAPERCYCLE; j++)
-{
-shared[tid             ] = g_indata[DATAPERCYCLE*j + THREAD_NUM*bix + index + tid           ];
-//__syncthreads();
-shared[tid+THREAD_NUM  ] = g_indata[DATAPERCYCLE*j + THREAD_NUM*bix + index + tid + THREAD_NUM];
-//__syncthreads();
-shared[tid+THREAD_NUM*2] = g_indata[DATAPERCYCLE*j + THREAD_NUM*bix + index + tid + THREAD_NUM*2];
-__syncthreads();
-
-#pragma unroll
-for(int k = 0; k < DATAPERCYCLE; k = k+1)
-{
-// dOut += x[(oldest + k+tid)% N] * h[j*DATAPERCYCLE + k];
-dOut += shared[k + tid] * h[j*DATAPERCYCLE + k];
-//dOut += shared[k + tid] * coeff_Kernel[j*DATAPERCYCLE + k];
-__syncthreads();
-}
-}
-__syncthreads();
-g_outdata[THREAD_NUM*bix + index + tid] = dOut;
-}
-}
- */
-
-
-
-/*
-//do FIR
-//each threads has offseted address to global memory. loop jumps threads*blocks.
-for (int index = 0; index < CalcSize; index = index + THREAD_NUM*bw)
-{
-dOut = 0.0;
-x[oldest]=g_indata[index];
-__syncthreads();
-
-//read g_indata to Shared Memory
-//cycle is, ex, 8=8192/1024.
-
-for (int j = 0; j < KERNELTAPS/DATAPERCYCLE; j++)
-{
-shared[tid             ] = g_indata[DATAPERCYCLE*j + THREAD_NUM*bix + index + tid           ];
-__syncthreads();
-shared[tid+THREAD_NUM  ] = g_indata[DATAPERCYCLE*j + THREAD_NUM*bix + index + tid + THREAD_NUM];
-__syncthreads();
-shared[tid+THREAD_NUM*2] = g_indata[DATAPERCYCLE*j + THREAD_NUM*bix + index + tid + THREAD_NUM*2];
-__syncthreads();
-
-#pragma unroll 16
-for(int k = 0; k < DATAPERCYCLE; k = k+1)
-{
-dOut += x[(oldest + k+tid)% N] * h[j*DATAPERCYCLE + k];
-//dOut += shared[k + tid] * coeff_Kernel[j*DATAPERCYCLE + k];
-__syncthreads();
-}
-}
-__syncthreads();
-g_outdata[THREAD_NUM*bix + index + tid] = dOut;
-}
-}
- */
-
-__device__ float xv[4][3], yv[4][3], xv2[4][3], yv2[4][3], gain, a1, a2, a3, b1, b2;
-
-__device__ void zerothem() {
-
-		for(int i=0;i<4;++i) {
-				for(int j=0;j<3;++j) {
-						xv[i][j]=0.0;
-						yv[i][j]=0.0;
-						xv2[i][j]=0.0;
-						yv2[i][j]=0.0;
-				}
+		for(i=0; i < idx; i++){
+			sr[i] = g_indata[x-i];
 		}
-}
+	}else if(x>=32 && x<2048){	// For other blocks
+		for(i=0; i<g_num_taps;i++){
+			sr[i] = g_indata[x-i];
+		}
+	}else
+		printf("a\n");
+	
+	__syncthreads();
 
-extern "C" void GPU_INIT() {
-		return;
-}
-
-__device__ int firstrun=0;
-
-__device__ float mult (float a, float b) {
-		return a*b;
-}
-
-__device__ void  device_memcpy (jack_default_audio_sample_t *to, jack_default_audio_sample_t *from, int len);
-
-__global__ void GPU_DSP(jack_default_audio_sample_t *ins, jack_default_audio_sample_t *outs, int count)
-{
-
-	/**
-	 * Gain with 2 threads
-	 *
-
-	 int i;
-	 for (i=0; i< count/2; i++) {
-	 if (threadIdx.x==0) {
-	 outs[i]=ins[i];
-	 } else if (threadIdx.x==1) {
-	 outs[i+count/2]=ins[i+count/2]; 
-	 }
-	 }
-	 */
-
-	/**
-	 * GAIN with 256 threads
-	 */
-	/*
-	   for (int i=0; i< count/BLOCKSIZE; i++) {
-	   outs[i+threadIdx.x*count/BLOCKSIZE]=0.1*ins[i+threadIdx.x*count/BLOCKSIZE];
-	   }
-	   }  
-	*/
-
-	int n=0;
-
-	if (firstrun==0){ 
-		gain=1.513365061e+03;
-		a1=1.0;
-		a2=2.0;
-		a3=1.0;
-		b1=-0.9286270861;
-		b2=1.9259839697;
-		zerothem();
-		firstrun=1;
-		__syncthreads();
+	// calculate
+	result = 0;
+	for(i = 0; i < g_num_taps; i++) {
+		result += sr[i] * g_taps[i];
 	}
 
-#pragma unroll 8
-	for (int i=0; i< count; i++) {
-		float intermediate = 0.0;
-		xv[n][0] = xv[n][1]; xv[n][1] = xv[n][2];
-		xv[n][2] = ins[i] / gain;
-		yv[n][0] = yv[n][1]; yv[n][1] = yv[n][2];
-		yv[n][2] =   (mult(a1,xv[n][0]) + mult(a3,xv[n][2])) + mult(a2,xv[n][1])
-				+ mult(b1,yv[n][0]) + (mult(b2,yv[n][1]));
-		__syncthreads();
-		intermediate = yv[n][2];
+	g_outdata[x] = result;	//g_indata[x];
+	__syncthreads();
 
-		xv2[n][0] = xv2[n][1]; xv2[n][1] = xv2[n][2];
-		xv2[n][2] = intermediate / gain;
-		yv2[n][0] = yv2[n][1]; yv2[n][1] = yv2[n][2];
-		yv2[n][2] =   (mult(a1,xv2[n][0]) + mult(a3,xv2[n][2])) + mult(a2,xv2[n][1])
-				+ (mult(b1,yv2[n][0])) + (mult(b2,yv2[n][1]));
-		__syncthreads();
-		outs[i]=yv2[n][2];
-	}
 }
 
-
-extern "C" void RunGPU_DSP( int grid, jack_default_audio_sample_t *ins, jack_default_audio_sample_t *outs, int count)
+extern "C" void RunGPU_DSP( jack_default_audio_sample_t *ins, jack_default_audio_sample_t *outs, jack_default_audio_sample_t *sr, jack_default_audio_sample_t *taps, int numtaps)
 {
-		/*
-		   if(first==0) {
-		   for (int j=0; j<N; j++) {
-		   x[j]=0.0;
-		   accum=0;
-		   impulseResponse<<< BLOCKS,THREADS >>>(j);
-		   h[j]=A[0]+accum;
-		   }
-		   first=1;
-		   }
-		 */
-		calcFIR<<< BLOCKS,THREAD_NUM >>>( ins, outs, count );
+	// count is nframes, ex) 2048 or 4096
+	//calcFIR<<< BLOCKS,THREAD_NUM >>>( ins, outs, count );
+	// 64, 32 - 2048
+	do_sample<<<BLOCKS, THREAD_NUM>>>(ins, outs, sr, taps, numtaps);
 }
-
-__device__ void device_memcpy (jack_default_audio_sample_t *to, 
-								jack_default_audio_sample_t *from, int len)
-{
-		int i;
-
-		for (i=0; i< len; i++) to[i] = from[i];
-
-}
-
